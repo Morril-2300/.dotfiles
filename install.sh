21537e658d7228b5158e54423c4a76b72d3ad6fe
Quinn <github@accounts.morril.de>
2026-01-27 16:09:19 +0100

    continue install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,17 @@
 
 sudo echo "Installing packages"
 
+sudo pacman -Syy
+
+#paru
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+
+paru -S yay-git
+
+sudo pacman -S archlinux-keyring
+yay -S hyprland-git
+sudo pacman -S waybar greetd-agreety gnome-keyring
+paru -S vscodium-bin
